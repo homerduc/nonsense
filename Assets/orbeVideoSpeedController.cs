c0d33b3fc29eb6b6e1e9a27a4b4a8671fc941434
homerduc <alexandre.duc00@orange.fr>
2025-05-15 19:42:52 +0200

    ça avance

--- Assets/orbeVideoSpeedController.cs
+++ Assets/orbeVideoSpeedController.cs
@@ -1,3 +1,5 @@
+using System.Xml;
+using TMPro;
 using UnityEngine;
 using UnityEngine.Video;
 
@@ -6,18 +8,44 @@ public class orbeVideoSpeedController : MonoBehaviour
     [SerializeField]
     AudioSource audioSource;
 
+    float volume;
+
     float startYCoord;
+
+    [SerializeField]
+    float maxVolumePoint = 4f;
+
+    float range;
+
+    // Différence/écart entre le maxVolumePoint et l'orbe
+    float diffAbsMaxVolPoint_Orbe;
+
+    float maxVolNeg;
+
+    [Range(0f, 1f)] public float textSoftness = 0.5f;
+    [SerializeField]
+    TMP_Text gettingItText;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         startYCoord = transform.position.y;
+        range = maxVolumePoint - startYCoord;
+
+        // Cloner le matériau pour ne pas modifier celui de toutes les instances
+        gettingItText.fontMaterial = new Material(gettingItText.fontMaterial);
     }
 
     // Update is called once per frame
     void Update()
     {
-        float volume = 0.1F * (transform.position.y - startYCoord);
-        audioSource.volume = volume;
+        diffAbsMaxVolPoint_Orbe = Mathf.Abs(maxVolumePoint - transform.position.y);
+
+        volume = 1 - (diffAbsMaxVolPoint_Orbe / range);
+        audioSource.volume = Mathf.Max(0, volume);
+
+        textSoftness = 1 - volume;
+        gettingItText.fontMaterial.SetFloat(ShaderUtilities.ID_OutlineSoftness, textSoftness);
+        gettingItText.fontMaterial.SetFloat(ShaderUtilities.ID_UnderlaySoftness, textSoftness);
     }
 }
